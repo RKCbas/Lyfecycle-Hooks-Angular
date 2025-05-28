d35bd10f72a7ca943c8905201f8732a4062ab877
RKCbas <sebashp2015@hotmail.com>
2025-05-27 18:00:31 -0600

    # Class 1 Basic configuration of files and routes

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
+  templateUrl: './navbar.component.html',
+  styles: `
+    nav {
+      display: flex;
+      gap: 1rem;
+      justify-content: center;
+      align-items: center;
+    }
+
+    .active {
+      color: #341162;
+      font-weight: bold;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavbarComponent {
+
+}
